eed872b8d0d549176031cca82683d121e66546d2
Milky Raven <heartonllamas@gmail.com>
2022-10-28 12:04:04 +0200

    updated promise to async await

--- index.js
+++ index.js
@@ -8,16 +8,15 @@ const data = require('./data');
 const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
 
 // Connection to the database "recipe-app"
-mongoose
-  .connect(MONGODB_URI)
-  .then(x => {
+const mongoConnect = async function() {
+  try{
+    const x = await mongoose.connect(MONGODB_URI)
     console.log(`Connected to the database: "${x.connection.name}"`);
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
-  })
-  .then(() => {
-    // Run your code here, after you have insured that the connection was made
-  })
-  .catch(error => {
-    console.error('Error connecting to the database', error);
-  });
+  } catch (err) {
+    console.log('Error connecting to the database' + err)
+  }
+}
+
+mongoConnect();
